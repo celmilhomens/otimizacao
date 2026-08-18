c4bb717370c513c3d490435871626af62230c877
celmilhomens <marcotulio@tuta.io>
2026-08-18 17:54:33 -0300

    Update 1.1-audio.sh

--- 1.1-audio.sh
+++ 1.1-audio.sh
@@ -3,3 +3,5 @@ set -euo pipefail
 
 # Áudio, Bluetooth e controle
 sudo pacman --needed -S pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack wireplumber alsa-utils alsa-tools alsa-plugins
+
+sudo reboot
