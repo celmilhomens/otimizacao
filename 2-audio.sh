26ade6bac20651db82fc22bceabb2b47773fcc73
celmilhomens <marcotulio@tuta.io>
2026-08-18 12:44:02 -0300

    2-audio.sh

---
+++ 2-audio.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Áudio, Bluetooth e controle
+sudo pacman --needed -S pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack wireplumber alsa-utils alsa-tools alsa-plugins pavucontrol bluez bluez-utils
