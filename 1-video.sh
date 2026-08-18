231a595526028336c40a14e4f43e5a8cb7c3819e
celmilhomens <marcotulio@tuta.io>
2026-08-18 17:17:23 -0300

    Update 1-video.sh

--- 1-video.sh
+++ 1-video.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# AMD, Vulkan e compatibilidade 32-bit
+# Instalando Mesa 26.3.0
+sudo pacman --noconfirm -U /home/milhomens/ssd-b/kernel-packages/mesa/26.3.0/*.pkg.tar.zst
+# Instalando outros componentes de video
 sudo pacman --needed -S mesa-utils vulkan-tools libva-utils corectrl vulkan-headers
 
 ./2-audio.sh
