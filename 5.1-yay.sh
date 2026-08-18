92490ef053f31c287d13d866562a30778bf6ad8d
celmilhomens <marcotulio@tuta.io>
2026-08-18 12:50:11 -0300

    5.1-yay.sh

--- 5.1-yay.sh
+++ 5.1-yay.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
 makepkg -si
