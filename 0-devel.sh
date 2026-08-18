43929cd3ae334e235fb53f2cccf4bc47fd1990af
celmilhomens <marcotulio@tuta.io>
2026-08-18 13:46:26 -0300

    Update 0-devel.sh

--- 0-devel.sh
+++ 0-devel.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Base, sistema e serviços
-sudo pacman --needed -S base-devel git linux-headers linux-firmware-amdgpu fastfetch htop btop zram-generator rtkit packagekit-qt6
+sudo pacman --needed -S base-devel git linux-headers linux-firmware-amdgpu fastfetch htop btop zram-generator
 
 sudo reboot
