2a9cf4f9bccc23d85c7c7f0c369700d8851daa85
celmilhomens <marcotulio@tuta.io>
2026-08-18 17:10:22 -0300

    Update 1-video.sh

--- 1-video.sh
+++ 1-video.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # AMD, Vulkan e compatibilidade 32-bit
-sudo pacman --needed -S mesa vulkan-radeon lib32-vulkan-radeon mesa-utils vulkan-tools libva-utils corectrl vulkan-headers
+sudo pacman --needed -S mesa-utils vulkan-tools libva-utils corectrl vulkan-headers
 
 ./2-audio.sh
