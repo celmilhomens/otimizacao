b214c32416b5861ca5acb8989e1d9ff3357b4973
celmilhomens <marcotulio@tuta.io>
2026-08-18 14:07:02 -0300

    Update 6-aur.sh

--- 6-aur.sh
+++ 6-aur.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo pacman -S onnxruntime
-paru --needed --noconfirm -S lug-helper protonplus brave-bin chatgpt-desktop vulkan-low-latency-layer
+paru --needed --noconfirm -S lug-helper protonplus brave-bin chatgpt-desktop vulkan-low-latency-layer ttf-ms-fonts
 paru -S opentrack zapzap
