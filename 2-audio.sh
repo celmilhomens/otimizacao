4ad769370c71051a8ace1b7867592c0a21072857
celmilhomens <marcotulio@tuta.io>
2026-08-18 13:07:35 -0300

    2-audio.sh

--- 2-audio.sh
+++ 2-audio.sh
@@ -2,3 +2,5 @@
 
 # Áudio, Bluetooth e controle
 sudo pacman --needed -S pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack wireplumber alsa-utils alsa-tools alsa-plugins pavucontrol bluez bluez-utils
+
+./3-kde.sh
