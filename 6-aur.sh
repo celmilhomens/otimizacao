8d761940b69a31f8de4d05b9aa7356278c2b56d7
celmilhomens <marcotulio@tuta.io>
2026-08-18 14:09:08 -0300

    Update 6-aur.sh

--- 6-aur.sh
+++ 6-aur.sh
@@ -3,3 +3,8 @@
 sudo pacman -S onnxruntime thunderbird pacman-contrib pacutils
 paru --needed --noconfirm -S lug-helper protonplus brave-bin chatgpt-desktop vulkan-low-latency-layer ttf-ms-fonts
 paru -S opentrack zapzap
+
+git clone https://github.com/dhruv8sh/arch-update-checker
+rm -rf ~/.local/share/plasma/plasmoids/org.kde.archupdatechecker/
+cp -r arch-update-checker/ ~/.local/share/plasma/plasmoids/org.kde.archupdatechecker
+systemctl --user restart plasma-plasmashell
