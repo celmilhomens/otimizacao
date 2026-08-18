1299f3242d4f11e9254f7ed46231cde3953bedb1
celmilhomens <marcotulio@tuta.io>
2026-08-18 17:53:00 -0300

    Update 0.0-basico.sh

--- 0.0-basico.sh
+++ 0.0-basico.sh
@@ -1,2 +1,66 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
+sudo pacman --needed --noconfirm -S reflector nano zram-generator gamemode lib32-gamemode base-devel linux-headers linux-firmware-amdgpu fastfetch htop
+
+### Configurando reflector
+sudo reflector --country Brazil --protocol https --latest 30 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Syy
+
+### Montando SSDs
+# Criando pastas
+cd /home/milhomens
+mkdir ssd-a
+mkdir ssd-b
+
+# Alterando fstab
+sudo tee -a /etc/fstab >/dev/null <<'EOF'
+/dev/nvme0n1p2  none  swap  defaults  0  0
+/dev/sda1  /home/milhomens/ssd-a  ext4  defaults,noatime  0  2
+/dev/sdb1  /home/milhomens/ssd-b  ext4  defaults,noatime  0  2
+EOF
+
+# Aplicando fstab
+sudo mount -a
+sudo swapon -a
+
+#Retornando
+cd /home/milhomens
+
+### Instalando Zram-Generator
+# Criando .conf
+sudo tee -a /etc/systemd/zram-generator.conf >/dev/null <<'EOF'
+[zram0]
+zram-size = 49152
+compression-algorithm = zstd
+EOF
+
+# Otimizando a conf
+sudo tee -a /etc/sysctl.d/99-vm-zram-parameters.conf >/dev/null <<'EOF'
+vm.swappiness = 180
+vm.page-cluster = 0
+vm.watermark_boost_factor = 0
+vm.watermark_scale_factor = 125
+vm.dirty_background_ratio = 3
+vm.dirty_ratio = 10
+vm.vfs_cache_pressure = 50
+vm.vfs_cache_pressure_denom = 100
+EOF
+
+# Aplicando a conf
+sudo sysctl --system
+
+### Instalando gamemode
+# Criando a conf
+sudo tee -a /etc/gamemode.ini >/dev/null <<'EOF'
+[general]
+desiredgov=performance
+desiredprof=performance
+softrealtime=off
+renice=10
+ioprio=0
+inhibit_screensaver=1
+disable_splitlock=0
+EOF
+
+
