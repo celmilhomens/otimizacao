a436279401e1db307581b9ccbb1be9f8248518e0
celmilhomens <marcotulio@tuta.io>
2026-08-18 13:43:31 -0300

    0.basica.sh

--- 0.basica.sh
+++ 0.basica.sh
@@ -1,22 +1,64 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
+### Montando SSDs
+# Criando pastas
 mkdir ssd-a
 mkdir ssd-b
 
+# Alterando fstab
 sudo tee -a /etc/fstab >/dev/null <<'EOF'
 /dev/nvme0n1p2  none  swap  defaults  0  0
 /dev/sda1  /home/milhomens/ssd-a  ext4  defaults,noatime  0  2
 /dev/sdb1  /home/milhomens/ssd-b  ext4  defaults,noatime  0  2
 EOF
 
+# Aplicando fstab
+sudo mount -a
+sudo swapon -a
 sudo systemctl enable --now fstrim.timer
 
+### Alterando Grub
+# Configurando o default grub
+sudo sed -i \
+  '/^GRUB_CMDLINE_LINUX_DEFAULT=/d;
+   /^GRUB_CMDLINE_LINUX=/d;
+   /^GRUB_DEFAULT=/d;
+   /^GRUB_SAVEDEFAULT=/d;
+   /^GRUB_TIMEOUT=/d;
+   /^GRUB_TIMEOUT_STYLE=/d;
+   /^GRUB_DISABLE_SUBMENU=/d' \
+  /etc/default/grub
+
+sudo tee -a /etc/default/grub >/dev/null <<'EOF'
+GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet zswap.enabled=0 amd_pstate=active mitigations=off transparent_hugepage=madvise"
+GRUB_CMDLINE_LINUX="amdgpu.ppfeaturemask=0xffffffff"
+GRUB_DEFAULT=saved
+GRUB_SAVEDEFAULT=true
+GRUB_TIMEOUT=2
+GRUB_TIMEOUT_STYLE=menu
+GRUB_DISABLE_SUBMENU=y
+EOF
+
+# Instalando Kernel 7.2
+sudo pacman --noconfirm -U /home/milhomens/ssd-b/kernel-packages/7.2/*.pkg.tar.zst
+
+# Subindo grub com mkconfig
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+### Montando Zram-Generator
+# Instalando zram-generator
+sudo pacman --noconfirm -S zram-generator
+
+# Criando .conf
 sudo tee -a /etc/systemd/zram-generator.conf >/dev/null <<'EOF'
 [zram0]
 zram-size = 49152
 compression-algorithm = zstd
 EOF
 
+### Otimizando swap/zswap
+# Criando a conf
 sudo tee -a /etc/sysctl.d/99-vm-zram-parameters.conf >/dev/null <<'EOF'
 vm.swappiness = 180
 vm.page-cluster = 0
@@ -28,6 +70,13 @@ vm.vfs_cache_pressure = 50
 vm.vfs_cache_pressure_denom = 100
 EOF
 
+# Aplicando a .conf
+sudo sysctl --system
+
+### Instalando gamemode
+sudo pacman --noconfirm -S gamemode lib32-gamemode
+
+# Criando a conf
 tee "$HOME/.config/gamemode.ini" >/dev/null <<'EOF'
 [general]
 desiredgov=performance
@@ -39,6 +88,4 @@ inhibit_screensaver=1
 disable_splitlock=0
 EOF
 
-sudo mount -a
-sudo swapon -a
-sudo sysctl --system
+./0-base.sh
