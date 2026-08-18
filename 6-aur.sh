6283c79ff3aada54e633d32fe065f5338a8fd976
celmilhomens <marcotulio@tuta.io>
2026-08-18 14:08:24 -0300

    Update 6-aur.sh

--- 6-aur.sh
+++ 6-aur.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo pacman -S onnxruntime thunderbird
+sudo pacman -S onnxruntime thunderbird pacman-contrib pacutils
 paru --needed --noconfirm -S lug-helper protonplus brave-bin chatgpt-desktop vulkan-low-latency-layer ttf-ms-fonts
 paru -S opentrack zapzap
