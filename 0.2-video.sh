58baae52189d912590545bb0704d3810141cd3d0
celmilhomens <marcotulio@tuta.io>
2026-08-19 14:04:06 -0300

    Update 0.2-video.sh

--- 0.2-video.sh
+++ 0.2-video.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 ### Instalando Mesa 26.3.0
 # Instalando pacote compilado
-sudo pacman --noconfirm -U /home/milhomens/ssd-b/linux/tkg-ready/mesa/26.3.0/*.pkg.tar.zst
+sudo pacman -U /home/milhomens/ssd-b/linux/tkg-ready/mesa/26.3.0/*.pkg.tar.zst
 
 # Instalando outros componentes de video
 sudo pacman --needed --noconfirm -S mesa-utils vulkan-tools libva-utils corectrl vulkan-headers
