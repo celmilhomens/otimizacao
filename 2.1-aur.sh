5d3baa884e5ea3561e1aa76c4f495452f430aea6
celmilhomens <marcotulio@tuta.io>
2026-09-08 11:42:11 -0300

    Update 2.1-aur.sh

--- 2.1-aur.sh
+++ 2.1-aur.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo pacman --needed -S thunderbird pacman-contrib pacutils onnxruntime
+sudo pacman --needed -S thunderbird pacman-contrib pacutils onnxruntime scx-tools cpupower hyperfine sysbench lm_sensors
 paru --needed --noconfirm -S lug-helper protonplus brave-bin chatgpt-desktop vulkan-low-latency-layer ttf-ms-fonts
 paru -S opentrack-git zapzap lsfg-vk-bin
 
