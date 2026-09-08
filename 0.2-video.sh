4e3d1f80a31dc3fc95774738852f5ea5b860ba10
celmilhomens <marcotulio@tuta.io>
2026-09-08 11:27:06 -0300

    Update 0.2-video.sh

--- 0.2-video.sh
+++ 0.2-video.sh
@@ -8,4 +8,16 @@ sudo pacman -U /home/milhomens/ssd-b/linux/tkg-ready/mesa/26.3.x/*.pkg.tar.zst
 # Instalando outros componentes de video
 sudo pacman --needed --noconfirm -S mesa-utils vulkan-tools libva-utils corectrl vulkan-headers
 
+sudo tee -a /etc/polkit-1/rules.d/90-corectrl.rules >/dev/null <<'EOF'
+polkit.addRule(function(action, subject) {
+    if ((action.id == "org.corectrl.helper.init" ||
+         action.id == "org.corectrl.helperkiller.init") &&
+        subject.local == true &&
+        subject.active == true &&
+        subject.isInGroup("milhomens")) {
+            return polkit.Result.YES;
+    }
+});
+EOF
+
 sudo reboot
